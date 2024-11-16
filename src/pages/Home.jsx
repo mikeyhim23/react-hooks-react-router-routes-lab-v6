4a761b9bf55018d053aa2d3578a022727d32659d
Christopher Karanja Nganga <christopher.nganga@student.moringaschool.com>
2024-11-16 10:55:12 +0300

    Routing lab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,33 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then(res => res.json())
+      .then(data => setMovies(data))
+  }, [])
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
+        <h1>Home Page</h1>
       </header>
       <main>
-        {/* Info goes here! */}
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <MovieCard title={movie.title} id={movie.id} />
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
-};
+}
 
 export default Home;
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams()
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then(res => res.json())
+      .then(data => setMovie(data))
+  }, [id])
+
+  if (!movie) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
+        <h1>{movie.title}</h1>
       </header>
       <main>
-        {/* Movie info here! */}
+        <p>{movie.time} minutes</p>
+        {movie.genres.map((genre, index) => (
+          <span key={index}>{genre}</span>
+        ))}
       </main>
     </>
-  );
-};
+  )
+}
 
 export default Movie;
